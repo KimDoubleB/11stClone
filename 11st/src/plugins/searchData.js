2494d81c02bed207c3de80a5442b487477026044
KimBB <tree9295@gmail.com>
2021-01-26 10:35:18 +0900

    fix: 환경변수 등록

--- 11st/src/plugins/searchData.js
+++ 11st/src/plugins/searchData.js
@@ -5,7 +5,7 @@ export default {
     // 전역으로 플러그인을 추가하는 것 -> 이제 $search를 통해 함수를 호출할 수 있다.
     install(Vue) {
         Vue.prototype.$search = async ({searchText}) => {
-            const {data} = await axios.get(`https://trusting-williams-8cacfb.netlify.app/.netlify/functions/search?apiKey=${1216}&searchText=${searchText}`)
+            const {data} = await axios.get(`https://trusting-williams-8cacfb.netlify.app/.netlify/functions/search?apiKey=${process.env.API_KEY}&searchText=${searchText}`)
             return data;
         }
     }
